dafc94c07649659af4ee844fba1c66c225145120
ChicoFromKentau <chingiz.alimov@gmail.com>
2022-12-13 00:42:22 +0600

    First commit

--- ColorizedApp/ViewController.swift
+++ ColorizedApp/ViewController.swift
@@ -9,51 +9,52 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet var colorizedView: UIView!
+//    @IBOutlet var colorizedView: UIView!
     
-    @IBOutlet var redColorValue: UILabel!
-    @IBOutlet var greenColorLValue: UILabel!
-    @IBOutlet var blueColorValue: UILabel!
-    
-    @IBOutlet var redColorSlider: UISlider!
-    @IBOutlet var greenColorSlider: UISlider!
-    @IBOutlet var blueColorSlider: UISlider!
+//    @IBOutlet var redColorValue: UILabel!
+//    @IBOutlet var greenColorLValue: UILabel!
+//    @IBOutlet var blueColorValue: UILabel!
+//
+//    @IBOutlet var redColorSlider: UISlider!
+//    @IBOutlet var greenColorSlider: UISlider!
+//    @IBOutlet var blueColorSlider: UISlider!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        colorizedView.layer.cornerRadius = 22
-        
-        setupRedSlider()
-        redColorValue.text = String(redColorSlider.value)
-        setupGreenSlider()
-        setupBlueSlider()
-        
-        
-        
+        //        colorizedView.layer.cornerRadius = 22
+        //
+        //        setupRedSlider()
+        //        redColorValue.text = String(redColorSlider.value)
+        //        setupGreenSlider()
+        //        setupBlueSlider()
+        //
+        //
+        //
+        //    }
+        //    private func setupRedSlider () {
+        //        redColorSlider.value = 0
+        //        redColorSlider.minimumTrackTintColor = .red
+        //        redColorSlider.minimumValue = 0
+        //        redColorSlider.maximumValue = 1
+        //
+        //    }
+        //    private func setupGreenSlider () {
+        //        greenColorSlider.value = 0
+        //        greenColorSlider.minimumTrackTintColor = .green
+        //        greenColorSlider.minimumValue = 0
+        //        greenColorSlider.maximumValue = 1
+        //
+        //
+        //    }
+        //    private func setupBlueSlider () {
+        //        blueColorSlider.value = 0
+        //        blueColorSlider.minimumTrackTintColor = .blue
+        //        blueColorSlider.minimumValue = 0
+        //        blueColorSlider.maximumValue = 1
+        //
+        //    }
+        //
+        //}
     }
-    private func setupRedSlider () {
-        redColorSlider.value = 0
-        redColorSlider.minimumTrackTintColor = .red
-        redColorSlider.minimumValue = 0
-        redColorSlider.maximumValue = 1
-        
-    }
-    private func setupGreenSlider () {
-        greenColorSlider.value = 0
-        greenColorSlider.minimumTrackTintColor = .green
-        greenColorSlider.minimumValue = 0
-        greenColorSlider.maximumValue = 1
-        
-        
-    }
-    private func setupBlueSlider () {
-        blueColorSlider.value = 0
-        blueColorSlider.minimumTrackTintColor = .blue
-        blueColorSlider.minimumValue = 0
-        blueColorSlider.maximumValue = 1
-        
-    }
-
 }
-

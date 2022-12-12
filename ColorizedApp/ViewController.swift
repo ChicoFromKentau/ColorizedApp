e669895f78bcdda4f4b74f585891ea3d2a764f91
ChicoFromKentau <chingiz.alimov@gmail.com>
2022-12-12 18:01:12 +0600

    Outlets setup

--- ColorizedApp/ViewController.swift
+++ ColorizedApp/ViewController.swift
@@ -8,12 +8,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
+
     @IBOutlet var colorizedView: UIView!
     
-    @IBOutlet var redColorLabel: UILabel!
-    @IBOutlet var greenColorLabel: UILabel!
-    @IBOutlet var blueColorLabel: UILabel!
+    @IBOutlet var redColorValue: UILabel!
+    @IBOutlet var greenColorLValue: UILabel!
+    @IBOutlet var blueColorValue: UILabel!
     
     @IBOutlet var redColorSlider: UISlider!
     @IBOutlet var greenColorSlider: UISlider!
@@ -22,9 +22,38 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        colorizedView.layer.cornerRadius = 22
+        
+        setupRedSlider()
+        redColorValue.text = String(redColorSlider.value)
+        setupGreenSlider()
+        setupBlueSlider()
+        
+        
         
     }
-
+    private func setupRedSlider () {
+        redColorSlider.value = 0
+        redColorSlider.minimumTrackTintColor = .red
+        redColorSlider.minimumValue = 0
+        redColorSlider.maximumValue = 1
+        
+    }
+    private func setupGreenSlider () {
+        greenColorSlider.value = 0
+        greenColorSlider.minimumTrackTintColor = .green
+        greenColorSlider.minimumValue = 0
+        greenColorSlider.maximumValue = 1
+        
+        
+    }
+    private func setupBlueSlider () {
+        blueColorSlider.value = 0
+        blueColorSlider.minimumTrackTintColor = .blue
+        blueColorSlider.minimumValue = 0
+        blueColorSlider.maximumValue = 1
+        
+    }
 
 }
 
